0144a12e33b35099ba94d17ce6a977d2f86bb521
Emedudu <cremmybanj7@gmail.com>
2025-12-05 07:52:44 +0100

    Deployed PositionManager, StateView

--- script/DeployPosm.s.sol
+++ script/DeployPosm.s.sol
@@ -5,30 +5,51 @@ import "forge-std/console2.sol";
 import "forge-std/Script.sol";
 
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
-import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
-import {Deploy, IPositionDescriptor, IPositionManager} from "../test/shared/Deploy.sol";
+// import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
+import {
+    Deploy,
+    IPositionDescriptor,
+    IPositionManager
+} from "../test/shared/Deploy.sol";
 import {IWETH9} from "../src/interfaces/external/IWETH9.sol";
 
 contract DeployPosmTest is Script {
     function setUp() public {}
 
-    function run(
-        address poolManager,
-        address permit2,
-        uint256 unsubscribeGasLimit,
-        address wrappedNative,
-        bytes32 nativeCurrencyLabelBytes
-    ) public returns (IPositionDescriptor positionDescriptor, IPositionManager posm) {
-        vm.startBroadcast();
+    function run()
+        public
+        returns (IPositionDescriptor positionDescriptor, IPositionManager posm)
+    {
+        address poolManager = 0xfE7582f6d3cb05c55c9DE5a612778D26e5F627a7;
+        address permit2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+        uint256 unsubscribeGasLimit = 300000;
+        address wrappedNative = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+        // bytes32 nativeCurrencyLabelBytes = "POL";
+        address positionDescriptor = 0x0000000000000000000000000000000000000000;
 
-        positionDescriptor = Deploy.positionDescriptor(poolManager, wrappedNative, nativeCurrencyLabelBytes, hex"00");
-        console2.log("PositionDescriptor", address(positionDescriptor));
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        //not sure we need a positionDescriptor for now
+        // positionDescriptor = Deploy.positionDescriptor(
+        //     poolManager,
+        //     wrappedNative,
+        //     nativeCurrencyLabelBytes,
+        //     hex"00"
+        // );
+        // console2.log("PositionDescriptor", address(positionDescriptor));
 
         posm = Deploy.positionManager(
-            poolManager, permit2, unsubscribeGasLimit, address(positionDescriptor), wrappedNative, hex"03"
+            poolManager,
+            permit2,
+            unsubscribeGasLimit,
+            address(positionDescriptor),
+            wrappedNative,
+            hex"03"
         );
         console2.log("PositionManager", address(posm));
 
         vm.stopBroadcast();
     }
 }
+//forge script script/DeployPosm.s.sol --rpc-url {rpcurl} --verify --ffi --broadcast
--- script/DeployStateView.s.sol
+++ script/DeployStateView.s.sol
@@ -9,8 +9,11 @@ import {Deploy, IStateView} from "../test/shared/Deploy.sol";
 contract DeployStateView is Script {
     function setUp() public {}
 
-    function run(address poolManager) public returns (IStateView state) {
-        vm.startBroadcast();
+    function run() public returns (IStateView state) {
+        address poolManager = 0xfE7582f6d3cb05c55c9DE5a612778D26e5F627a7;
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         // forge script --broadcast --sig 'run(address)' --rpc-url <RPC_URL> --private-key <PRIV_KEY> --verify script/DeployStateView.s.sol:DeployStateView <POOL_MANAGER_ADDR>
         state = Deploy.stateView(poolManager, hex"00");
@@ -20,3 +23,5 @@ contract DeployStateView is Script {
         vm.stopBroadcast();
     }
 }
+
+//forge script script/DeployStateView.s.sol --rpc-url {rpcurl} --verify --ffi --broadcast
